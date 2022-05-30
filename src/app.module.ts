d292a158382b81cf5b7df1ad5d89f1e61c6502c2
EndersonMarques <6336.cara@gmail.com>
2022-05-30 13:41:07 -0300

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,26 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost' || 'ec2-3-234-131-8.compute-1.amazonaws.com',
+    //   port: 5432,
+    //   username: 'postgres' || 'gwlddzbrbqbqkg',
+    //   password:
+    //     '123456' ||
+    //     'ff32e59dcd130fd82bd2b8a4b0983114466e3ba6b1b70970e965d6c8ac90b50f',
+    //   database: 'task_database' || 'd2aj1nk5hsc9lr',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost' || 'ec2-3-234-131-8.compute-1.amazonaws.com',
+      host: 'ec2-3-234-131-8.compute-1.amazonaws.com',
       port: 5432,
-      username: 'postgres' || 'gwlddzbrbqbqkg',
+      username: 'gwlddzbrbqbqkg',
       password:
-        '123456' ||
         'ff32e59dcd130fd82bd2b8a4b0983114466e3ba6b1b70970e965d6c8ac90b50f',
-      database: 'task_database' || 'd2aj1nk5hsc9lr',
+      database: 'd2aj1nk5hsc9lr',
       autoLoadEntities: true,
       synchronize: true,
     }),
